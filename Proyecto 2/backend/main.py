e069c1f3273b37ad9d1549bc8a4a8ba10c91d82e
kren007 <122758347+KrenMora@users.noreply.github.com>
2023-10-28 00:27:34 -0600

    ADD mongodb :+1:

--- Proyecto 2/backend/main.py
+++ Proyecto 2/backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import mysql.connector
+import pymongo
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
@@ -8,12 +10,21 @@ CORS(app)
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="140620",
-    database="bd2_prac2"
+    password="root123",
+    database="bd2_p1"
 )
 cursor = db.cursor()
 
+# Configuracion conexion a MongoDB
+mongodb_url = "mongodb+srv://admin:admin123@cluster0.i9abrxz.mongodb.net/"
+# Establece la conexión a la base de datos
+client = pymongo.MongoClient(mongodb_url)
+# Selecciona la base de datos
+dbMongo = client['Proyecto1']
+
 # Endpoint para consultas GET
+
+
 @app.route('/data/habitaciones', methods=['GET'])
 def get_data_h():
     query = "SELECT * FROM habitacion;"
@@ -21,8 +32,9 @@ def get_data_h():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'idHabitacion' : row[0], 'habitacion' : row[1]})
+        rows.append({'idHabitacion': row[0], 'habitacion': row[1]})
     return jsonify(rows)
+
 
 @app.route('/data/pacientes', methods=['GET'])
 def get_data_p():
@@ -31,10 +43,12 @@ def get_data_p():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'idPaciente' : row[0]})
+        rows.append({'idPaciente': row[0]})
     return jsonify(rows)
 
 # Reporte 1 - MYSQL
+
+
 @app.route('/mysql/reporte/1', methods=['GET'])
 def get_mysql_reporte_1():
     query = """
@@ -52,10 +66,12 @@ def get_mysql_reporte_1():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'categoria' : row[0], 'total_pacientes' : row[1]})
+        rows.append({'categoria': row[0], 'total_pacientes': row[1]})
     return jsonify(rows)
 
 # Reporte 2 - MYSQL
+
+
 @app.route('/mysql/reporte/2', methods=['GET'])
 def get_mysql_reporte_2():
     query = """
@@ -70,10 +86,12 @@ def get_mysql_reporte_2():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'habitacion' : row[0], 'pacientes_en_habitacion' : row[1]})
+        rows.append({'habitacion': row[0], 'pacientes_en_habitacion': row[1]})
     return jsonify(rows)
 
 # Reporte 3 - MYSQL
+
+
 @app.route('/mysql/reporte/3', methods=['GET'])
 def get_mysql_reporte_3():
     query = """
@@ -86,10 +104,12 @@ def get_mysql_reporte_3():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'genero' : row[0], 'total_pacientes' : row[1]})
+        rows.append({'genero': row[0], 'total_pacientes': row[1]})
     return jsonify(rows)
 
 # Reporte 4 - MYSQL
+
+
 @app.route('/mysql/reporte/4', methods=['GET'])
 def get_mysql_reporte_4():
     query = """
@@ -107,10 +127,12 @@ def get_mysql_reporte_4():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'edad' : row[0], 'total_atendidos' : row[1]})
+        rows.append({'edad': row[0], 'total_atendidos': row[1]})
     return jsonify(rows)
 
 # Reporte 5 - MYSQL
+
+
 @app.route('/mysql/reporte/5', methods=['GET'])
 def get_mysql_reporte_5():
     query = """
@@ -128,10 +150,12 @@ def get_mysql_reporte_5():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'edad' : row[0], 'total_atendidos' : row[1]})
+        rows.append({'edad': row[0], 'total_atendidos': row[1]})
     return jsonify(rows)
 
 # Reporte 6 - MYSQL
+
+
 @app.route('/mysql/reporte/6', methods=['GET'])
 def get_mysql_reporte_6():
     query = """
@@ -148,10 +172,12 @@ def get_mysql_reporte_6():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'habitacion' : row[0], 'pacientes_en_habitacion' : row[1]})
+        rows.append({'habitacion': row[0], 'pacientes_en_habitacion': row[1]})
     return jsonify(rows)
 
 # Reporte 7 - MYSQL
+
+
 @app.route('/mysql/reporte/7', methods=['GET'])
 def get_mysql_reporte_7():
     query = """
@@ -168,10 +194,12 @@ def get_mysql_reporte_7():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'habitacion' : row[0], 'pacientes_en_habitacion' : row[1]})
+        rows.append({'habitacion': row[0], 'pacientes_en_habitacion': row[1]})
     return jsonify(rows)
 
 # Reporte 8 - MYSQL
+
+
 @app.route('/mysql/reporte/8', methods=['GET'])
 def get_mysql_reporte_8():
     query = """
@@ -186,10 +214,11 @@ def get_mysql_reporte_8():
     result = cursor.fetchall()
     rows = []
     for row in result:
-        rows.append({'fecha' : row[0], 'pacientes' : row[1]})
+        rows.append({'fecha': row[0], 'pacientes': row[1]})
     return jsonify(rows)
 
 # Endpoint para consultas POST
+
 @app.route('/query', methods=['POST'])
 def post_query():
     data = request.get_json()
@@ -197,6 +226,268 @@ def post_query():
     cursor.execute(query)
     result = cursor.fetchall()
     return jsonify(result)
+
+
+# Reporte 1 - MongoDB
+@app.route('/mongodb/reporte/1', methods=['GET'])
+def get_mongo_reporte_1():
+    coleccion = dbMongo['paciente']
+    query = MongoC(1)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+# Reporte 2 - MongoDB
+@app.route('/mongodb/reporte/2', methods=['GET'])
+def get_mongo_reporte_2():
+    coleccion = dbMongo['habitacion']
+    query = MongoC(2)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+# Reporte 3 - MongoDB
+@app.route('/mongodb/reporte/3', methods=['GET'])
+def get_mongo_reporte_3():
+    coleccion = dbMongo['paciente']
+    query = MongoC(3)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+# Reporte 4 - MongoDB
+@app.route('/mongodb/reporte/4', methods=['GET'])
+def get_mongo_reporte_4():
+    coleccion = dbMongo['paciente']
+    query = MongoC(4)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+# Reporte 5 - MongoDB
+@app.route('/mongodb/reporte/5', methods=['GET'])
+def get_mongo_reporte_5():
+    coleccion = dbMongo['paciente']
+    query = MongoC(5)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+# Reporte 6 - MongoDB
+@app.route('/mongodb/reporte/6', methods=['GET'])
+def get_mongo_reporte_6():
+    coleccion = dbMongo['habitacion']
+    query = MongoC(6)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+# Reporte 7 - MongoDB
+@app.route('/mongodb/reporte/7', methods=['GET'])
+def get_mongo_reporte_7():
+    coleccion = dbMongo['habitacion']
+    query = MongoC(7)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+# Reporte 8 - MongoDB
+@app.route('/mongodb/reporte/8', methods=['GET'])
+def get_mongo_reporte_8():
+    coleccion = dbMongo['log_actividad']
+    query = MongoC(8)
+    resultados = coleccion.aggregate(query)
+    return jsonify(list(resultados))
+
+
+def MongoC(rep):
+    print("Hola desde mi_funcion")
+    pipeline = []
+    # Defina la consulta
+    if rep == 1:
+        pipeline = [
+            {
+                '$group': {
+                    '_id': {
+                        '$switch': {
+                            'branches': [
+                                {
+                                    'case': {
+                                        '$lt': [
+                                            '$edad', 18
+                                        ]
+                                    },
+                                    'then': 'Pediátrico'
+                                }, {
+                                    'case': {
+                                        '$and': [
+                                            {
+                                                '$gte': [
+                                                    '$edad', 18
+                                                ]
+                                            }, {
+                                                '$lte': [
+                                                    '$edad', 60
+                                                ]
+                                            }
+                                        ]
+                                    },
+                                    'then': 'Mediana Edad'
+                                }, {
+                                    'case': {
+                                        '$gt': [
+                                            '$edad', 60
+                                        ]
+                                    },
+                                    'then': 'Geriátrico'
+                                }
+                            ],
+                            'default': 'Desconocido'
+                        }
+                    },
+                    'total': {
+                        '$sum': 1
+                    }
+                }
+            }
+        ]
+    elif rep == 2:
+        pipeline = [
+            {
+                '$lookup': {
+                    'from': 'log_actividad',
+                    'localField': 'idHabitacion',
+                    'foreignField': 'idHabitacion',
+                    'as': 'actividades'
+                }
+            }, {
+                '$project': {
+                    '_id': 0,
+                    'habitacion': 1,
+                    'pacientes_en_habitacion': {
+                        '$size': '$actividades'
+                    }
+                }
+            }
+        ]
+    elif rep == 3:
+        pipeline = [
+            {
+                '$group': {
+                    '_id': '$genero',
+                    'total': {
+                        '$sum': 1
+                    }
+                }
+            }
+        ]
+    elif rep == 4:
+        pipeline = [
+            {
+                '$group': {
+                    '_id': '$edad',
+                    'total': {
+                        '$sum': 1
+                    }
+                }
+            }, {
+                '$sort': {
+                    'total': -1
+                }
+            }, {
+                '$limit': 5
+            }
+        ]
+    elif rep == 5:
+        pipeline = [
+            {
+                '$group': {
+                    '_id': '$edad',
+                    'total': {
+                        '$sum': 1
+                    }
+                }
+            }, {
+                '$sort': {
+                    'total': 1
+                }
+            }, {
+                '$limit': 5
+            }
+        ]
+    elif rep == 6:
+        pipeline = [
+            {
+                '$lookup': {
+                    'from': 'log_actividad',
+                    'localField': 'idHabitacion',
+                    'foreignField': 'idHabitacion',
+                    'as': 'actividades'
+                }
+            }, {
+                '$project': {
+                    '_id': 0,
+                    'habitacion': 1,
+                    'pacientes_en_habitacion': {
+                        '$size': '$actividades'
+                    }
+                }
+            }, {
+                '$sort': {
+                    'pacientes_en_habitacion': -1
+                }
+            }, {
+                '$limit': 5
+            }
+        ]
+    elif rep == 7:
+        pipeline = [
+            {
+                '$lookup': {
+                    'from': 'log_actividad',
+                    'localField': 'idHabitacion',
+                    'foreignField': 'idHabitacion',
+                    'as': 'actividades'
+                }
+            }, {
+                '$project': {
+                    '_id': 0,
+                    'habitacion': 1,
+                    'pacientes_en_habitacion': {
+                        '$size': '$actividades'
+                    }
+                }
+            }, {
+                '$sort': {
+                    'pacientes_en_habitacion': 1
+                }
+            }, {
+                '$limit': 5
+            }
+        ]
+    elif rep == 8:
+        pipeline = [
+            {
+                '$group': {
+                    '_id': {
+                        '$dateToString': {
+                            'format': '%Y-%m-%d',
+                            'date': '$timestamp'
+                        }
+                    },
+                    'totalPacientes': {
+                        '$sum': 1
+                    }
+                }
+            }, {
+                '$sort': {
+                    'totalPacientes': -1
+                }
+            }, {
+                '$limit': 1
+            }, {
+                '$project': {
+                    '_id': 0,
+                    'fecha': '$_id',
+                    'totalPacientes': 1
+                }
+            }
+        ]
+    return pipeline
+
 
 if __name__ == '__main__':
     app.run(debug=True)
